b6329fbfbc79322a1d0b61d37d3ec4341e6ec23f
Ninad262002 <ninadmahure@gmail.com>
2026-07-30 18:33:41 +0530

    day 5

---
+++ 05-Day-5 - Comments, Escape Sequences & Print Statement/main.py
@@ -0,0 +1,13 @@
+# This is a comment in the code 
+print("Hello\nWorld!")  #escape sequence for new line
+print("Hello this is a \"sample test \" for your final exam")  #escape sequence for double quotes
+print("Hello this is a sample test \' for your final exam")  #escape sequence for single quotes
+
+
+"""
+Hi my name is ninad mahure and i have accepted the 100 days code challange 
+and today is my fifth day and i am learning multi line comments in python
+"""
+
+print("hello", 4, 5, 6, 7, sep="/")
+print("hello", 4, 5, 6, 7, sep="/", end="ninad")
