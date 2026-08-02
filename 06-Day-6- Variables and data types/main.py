b83d3a4cd11f57c371ce25ef3b56095cf15f3474
Ninad262002 <ninadmahure@gmail.com>
2026-08-02 22:18:18 +0530

    Day 7

---
+++ 06-Day-6- Variables and data types/main.py
@@ -0,0 +1,5 @@
+b = "Harry"
+print(b)
+a = "1"
+print(type(a))
+print(a+b)
---
+++ 07-Day-7- Create a calculator/main.py
@@ -0,0 +1,8 @@
+num1=5
+num2=11
+print("The sum of", num1, "and", num2, "is", num1 + num2)
+print("The difference of", num1, "and", num2, "is", num1 - num2)
+print("The product of", num1, "and", num2, "is", num1*num2)
+print("The quotient of", num1, "and", num2, "is", num1/num2)
+print("The remainder of", num1, "and", num2, "is", num1%num2)
+print("the double division of", num1, "and", num2, "is", num1//num2)
