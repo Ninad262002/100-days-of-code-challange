745f58b5c6c7ecebd9e310229a9f7e9ac0edbcb3
Ninad262002 <ninadmahure@gmail.com>
2026-08-05 18:47:30 +0530

    Day 10

---
+++ 09-Day-9 Typecasting/main.py
@@ -0,0 +1,11 @@
+# Explicit TypeCasting
+a= "1"
+b = "2"
+print(int(a) + int(b))
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+e = c + d
+print(e)
+print(type(e))
---
+++ 10-Day-10 User Input in Python/main.py
@@ -0,0 +1,8 @@
+# a = input("Enter you name:")
+# print("My name is ", a)
+
+b = float(input("Enter first number: "))
+c = int(input("Enter second number: "))
+print(c + b)
+print(int(c) + int(b))
+
